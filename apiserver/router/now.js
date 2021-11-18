8b66c473e54727c9c248eb4b3ae4518e8198fa81
HDIK <1023611905@qq.com>
2021-11-18 22:20:13 +0800

    完成电影数据查询接口

---
+++ apiserver/router/now.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = express.Router();
+module.exports = router;
+
+const db = require('../db')
+const {formatData} = require('../utils')
+
+//全部查询
+// get /api/now/list
+router.get('/list',async (req,res)=>{
+    let sql = `select * from now`
+
+    // console.log(req.query);
+    const {category}=req.query
+    // console.log(category);
+    if(category){
+        sql+=` where category="${category}"`
+    }
+
+
+    const data = await db(sql)
+    // res.send(formatData({data}))
+    res.send(formatData.success(data))
+})
+
+// post /api/now
+router.post('/',(req,res)=>{
+
+    res.send()
+})
+
+// delete /api/now/3
+router.delete('/:id',(req,res)=>{
+
+    res.send()
+})
+
+// put /api/now/3
+router.put('/:id',(req,res)=>{
+
+    res.send()
+})
+
+//通过id查询
+// get /api/now/3
+router.get('/:id',async(req,res)=>{
+    // console.log(req.params);
+    const {id}=req.params
+    // console.log(id);
+
+    const sql=`select *from now where id='${id}'`
+    const data=await db(sql)
+
+    res.send(formatData.success(data))
+
+})
